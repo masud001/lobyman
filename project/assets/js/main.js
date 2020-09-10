09d30425917d057f041ffc887b34f73502d18f1e
masud001 <madhnagar@gmail.com>
2020-09-11 05:22:38 +0600

    initial comments

--- project/assets/js/main.js
+++ project/assets/js/main.js
@@ -100,11 +100,11 @@ $(document).ready(function () {
 		},
 	});
 	// hero slider for home page start
-	// blog_sliderfor home page end
+	// blog_slider for home page end
 	$(".blog_slider").owlCarousel({
 		loop: true,
 		autoplay: false,
-		dots: true,
+		dots: false,
 		nav: true,
 		navText: [
 			"<i class='fas fa-arrow-left'></i>",
@@ -117,12 +117,128 @@ $(document).ready(function () {
 				items: 1,
 			},
 			600: {
-				items: 4,
+				items: 1,
 			},
 			1000: {
 				items: 3,
 			},
 		},
 	});
-	// hero slider for home page start
+	// blog_slider for home page start
+	// arrived_slider home page end
+	$(".arrived_slider").owlCarousel({
+		loop: true,
+		autoplay: false,
+		dots: false,
+		nav: true,
+		navText: [
+			"<i class='fas fa-arrow-left'></i>",
+			"<i class='fas fa-arrow-right'></i>",
+		],
+		margin: 15,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 3,
+			},
+		},
+	});
+	// arrived_slider for home page start
+	// wine_of_weeks home page end
+	$(".wine_of_weeks_slider").owlCarousel({
+		loop: true,
+		autoplay: false,
+		dots: false,
+		nav: true,
+		navText: [
+			"<i class='fas fa-arrow-left'></i>",
+			"<i class='fas fa-arrow-right'></i>",
+		],
+		margin: 15,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 3,
+			},
+		},
+	});
+	// wine_of_weeks for home page start
+	// wine_advocate_slider home page end
+	$(".wine_advocate_slider").owlCarousel({
+		loop: true,
+		autoplay: false,
+		dots: false,
+		nav: true,
+		navText: [
+			"<i class='fas fa-arrow-left'></i>",
+			"<i class='fas fa-arrow-right'></i>",
+		],
+		margin: 15,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 3,
+			},
+		},
+	});
+	// wine_advocate_slider for home page start
+
+	// video play button youtube video play
+
+	//
+	$(".video_play_btn").magnificPopup({
+		type: "iframe",
+		iframe: {
+			markup:
+				'<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				"</div>", // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+			patterns: {
+				youtube: {
+					index: "youtube.com/", // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+					id: "v=", // String that splits URL in a two parts, second part should be %id%
+					// Or null - full URL will be returned
+					// Or a function that should return %id%, for example:
+					// id: function(url) { return 'parsed id'; }
+
+					src: "//www.youtube.com/embed/%id%?autoplay=1", // URL that will be set as a source for iframe.
+				},
+				vimeo: {
+					index: "vimeo.com/",
+					id: "/",
+					src: "//player.vimeo.com/video/%id%?autoplay=1",
+				},
+				gmaps: {
+					index: "//maps.google.",
+					src: "%id%&output=embed",
+				},
+
+				// you may add here more sources
+			},
+
+			srcAction: "iframe_src", // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+		},
+		// other options
+	});
 });
